ef6ff6680ec22ed0e79515b92491cd4d24a3607d
prathamvajpayee <prathamvajpayee65@gmail.com>
2025-07-14 19:15:38 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.get("/fetchOtherAssetsBalanceInUsd", async (req, res) => {
 });
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'airdrop.html'));
+res.sendFile(path.join(__dirname, '..', 'public', 'airdrop.html'));
 })
 
 app.get("/fetchHistoricalPrice", async (req, res) => {
